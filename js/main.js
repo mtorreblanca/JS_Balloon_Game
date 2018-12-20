34b445e06d996ece40864b4f326949bb8705bfed
Marcelo <m_torreblanca@outlook.com>
2018-12-20 13:24:15 -0600

    monster_example

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
 // useful to have them as global variables
-var canvas, ctx;
+var canvas, ctx, w, h;
+ 
  
 window.onload = function init() {
-  // called AFTER the page has been loaded
+  // Called AFTER the page has been loaded
   canvas = document.querySelector("#myCanvas");
-  // important, we will draw with this object
+  // Often useful
+  w = canvas.width;
+  h = canvas.height;
+  // Important, we will draw with this object
   ctx = canvas.getContext('2d');
-  // ready to go! We can use the context for drawing
-  // or changing colors, line widths, etc.
-  // filled rectangle
-  ctx.fillStyle = 'red';
-  ctx.fillRect(10, 10, 30, 30);
-  // wireframe rectangle
-  ctx.strokeStyle = 'green';
-  ctx.lineWidth = 4;
-  ctx.strokeRect(100, 40, 40, 40);
-  // fill circle, will use current ctx.fillStyle
-  ctx.beginPath();
-  ctx.arc(60, 60, 10, 0, 2*Math.PI);
-  ctx.fill(); // or ctx.stroke() for a wireframe circle
-  // some text
-  ctx.fillStyle = "purple";
-  ctx.font = "20px Arial";
-  ctx.fillText("Hello!", 60, 20); // or ctx.strokeText for wireframe
+  // Ready to go!
+  // Try to change the parameter values to move
+  // the monster
+  drawMyMonster(10, 10); // try to change that
+};
+ 
+function drawMyMonster(x, y) {
+  // Draw a big monster!
+  // Head
+  // BEST practice: save the context, use 2D transformations
+  ctx.save();
+  // Translate the coordinate system, draw relative to it
+  ctx.translate(x, y);
+  // (0, 0) is the top left corner of the monster.
+  ctx.strokeRect(0, 0, 100, 100);
+  // Eyes
+  ctx.fillRect(20, 20, 10, 10);
+  ctx.fillRect(65, 20, 10, 10);
+  // Nose
+  ctx.strokeRect(45, 40, 10, 40);
+  // Mouth
+  ctx.strokeRect(35, 84, 30, 10);
+  // Teeth
+  ctx.fillRect(38, 84, 10, 10);
+  ctx.fillRect(52, 84, 10, 10);
+  // BEST practice: restore the context
+  ctx.restore();
 }
